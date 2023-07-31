e15bd80617802d782589a6e69afc41f6ac83af74
jshong0907 <jshong0907@gmail.com>
2023-07-31 17:08:11 +0900

    Feature: socket chatting server config

---
+++ src/leporem_art_chatting/server.py
@@ -0,0 +1,82 @@
+import asyncio
+import logging
+
+import requests
+import socketio
+
+from leporem_art_chatting.settings import LOGIN_URL, MESSAGE_UPLOAD_URL
+
+logger = logging.getLogger(__name__)
+sio = socketio.AsyncServer(async_mode='asgi')
+app = socketio.ASGIApp(sio)
+
+
+@sio.event
+async def connect(sid, environ):
+    logger.info(f'connect {sid}')
+
+
+@sio.event
+async def authenticate(sid, data):
+    response = requests.post(LOGIN_URL, data={'id_token': data['id_token']})
+    if not response.status_code == 200:
+        await sio.disconnect(sid)
+        return
+
+    user_id = response.json()['user_id']
+    logging.info(f'authenticate {user_id}')
+
+    async with sio.session(sid) as session:
+        session['is_authenticated'] = True
+        session['id_token'] = data['id_token']
+        session['user_id'] = user_id
+        sio.enter_room(sid, user_id)
+
+
+async def _validate_session_login(sid):
+    async with sio.session(sid) as session:
+        if not session['is_authenticated']:
+            await sio.disconnect(sid)
+
+
+@sio.event
+async def send_message(sid, data):
+    await _validate_session_login(sid)
+    send_data = {
+        'message': data['message'],
+        'chat_room_id': data['chat_room_id'],
+     }
+    await asyncio.gather(
+        sio.emit('receive_message', send_data, room=data['opponent_user_id']),
+        _migrate_message(sid, data),
+    )
+    logger.info(f'send_message {data}')
+
+
+async def _migrate_message(sid, data):
+    async with sio.session(sid) as session:
+        headers = {
+            'Authorization': f'Palindrome {session["id_token"]}',
+        }
+        send_data = {
+            'chat_room_id': data['chat_room_id'],
+            'text': data['message'],
+        }
+
+        response = requests.post(MESSAGE_UPLOAD_URL, data=send_data, headers=headers)
+        if not response.status_code == 201:
+            logger.error(response.status_code, response.text)
+
+
+@sio.event
+async def disconnect(sid):
+    logger.info(f'disconnect {sid}')
+
+
+def run_server():
+    import uvicorn
+    uvicorn.run(app, host='0.0.0.0', port=8000, log_level='info')
+
+
+if __name__ == '__main__':
+    run_server()
---
+++ src/leporem_art_chatting/settings.py
@@ -0,0 +1,3 @@
+BASE_API_URL = 'http://localhost:8001'
+LOGIN_URL = BASE_API_URL + '/users/login/kakao'
+MESSAGE_UPLOAD_URL = BASE_API_URL + '/chats/messages'
