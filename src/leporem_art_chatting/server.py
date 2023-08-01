e9c679dee5946b39403d0ee90be062dcceed8ea1
jshong0907 <jshong0907@gmail.com>
2023-08-01 12:24:24 +0900

    Fix: change port to 80

--- src/leporem_art_chatting/server.py
+++ src/leporem_art_chatting/server.py
@@ -75,7 +75,7 @@ async def disconnect(sid):
 
 def run_server():
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8000, log_level='info')
+    uvicorn.run(app, host='0.0.0.0', port=80, log_level='info')
 
 
 if __name__ == '__main__':
--- src/leporem_art_chatting/settings.py
+++ src/leporem_art_chatting/settings.py
@@ -1,3 +1,3 @@
-BASE_API_URL = 'http://localhost:8001'
+BASE_API_URL = 'https://dev.leporem.art'
 LOGIN_URL = BASE_API_URL + '/users/login/kakao'
 MESSAGE_UPLOAD_URL = BASE_API_URL + '/chats/messages'
