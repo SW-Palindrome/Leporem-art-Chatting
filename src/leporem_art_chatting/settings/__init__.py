962498fa0e53c6b23bbb2bf77f43143f9400557e
jshong0907 <jshong0907@gmail.com>
2023-08-08 13:19:40 +0900

    Feature: settings separate

--- src/leporem_art_chatting/settings/__init__.py
+++ src/leporem_art_chatting/settings/__init__.py
@@ -1,4 +1,10 @@
-BASE_API_URL = 'https://dev.leporem.art'
+import os
+
+if os.getenv('ENV') == 'dev':
+    from .develop import *
+elif os.getenv('ENV') == 'prod':
+    from .production import *
+
 LOGIN_URL = BASE_API_URL + '/users/login/kakao'
 MESSAGE_UPLOAD_URL = BASE_API_URL + '/chats/messages'
 CHATROOM_CREATE_BY_BUYER_URL = BASE_API_URL + '/chats/buyer'
